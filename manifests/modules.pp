3df10f37c66f3da0cfbd84af29cbeb36601cef00
Thomas Cassaert <tcassaert@inuits.eu>
2019-01-14 10:05:08 +0100

    Added option to define custom config hash for modules

--- manifests/modules.pp
+++ manifests/modules.pp
@@ -7,16 +7,28 @@
 # @example
 #   filebeat::modules { 'namevar': }
 define filebeat::modules(
-  Enum['enable', 'disable'] $action = 'enable',
-  String                    $module = $title,
-  String                    $filebeat_module_dir = '/etc/filebeat/modules.d',
+  Enum['enable', 'disable'] $action                 = 'enable',
+  String                    $module                 = $title,
+  String                    $filebeat_module_dir    = '/etc/filebeat/modules.d',
 
   # Apache2 module variables
   Boolean                   $apache2_access_enabled = false,
-  Array[String]             $apache2_access_paths = [],
-  Boolean                   $apache2_error_enabled = false,
-  Array[String]             $apache2_error_paths = [],
+  Array[String]             $apache2_access_paths   = [],
+  Optional[Hash]            $apache2_config_hash    = undef,
+  Boolean                   $apache2_error_enabled  = false,
+  Array[String]             $apache2_error_paths    = [],
+
+  # Nginx module variables
+  Boolean                   $nginx_access_enabled   = false,
+  Array[String]             $nginx_access_paths     = [],
+  Optional[Hash]            $nginx_config_hash      = undef,
+  Boolean                   $nginx_error_enabled    = false,
+  Array[String]             $nginx_error_paths      = [],
 ) {
+
+  if ! defined(Class['filebeat']) {
+      fail('You must include the filebeat base class before using any filebeat defined resources')
+  }
 
   if $action == 'enable' {
     file { "${filebeat_module_dir}/${module}.yml":
