aa3ff9515a02462a52f0a76d3f067a6640e47c73
Thomas Cassaert <tcassaert@inuits.eu>
2019-01-14 11:05:33 +0100

    Added option to loadbalance logstash output

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,15 +7,16 @@
 # @example
 #   include filebeat::config
 class filebeat::config (
-  $config_dir          = $::filebeat::config_dir,
-  $config_reload       = $::filebeat::config_reload,
-  $elasticsearch_hosts = $::filebeat::elasticsearch::hosts,
-  $inputs              = $::filebeat::inputs,
-  $inputs_location     = $::filebeat::inputs_location,
-  $home_path           = $::filebeat::home_path,
-  $logstash_hosts      = $::filebeat::logstash_hosts,
-  $modules_location    = $::filebeat::modules_location,
-  $shipper_name        = $::filebeat::shipper_name,
+  $config_dir           = $::filebeat::config_dir,
+  $config_reload        = $::filebeat::config_reload,
+  $elasticsearch_hosts  = $::filebeat::elasticsearch::hosts,
+  $inputs               = $::filebeat::inputs,
+  $inputs_location      = $::filebeat::inputs_location,
+  $home_path            = $::filebeat::home_path,
+  $logstash_hosts       = $::filebeat::logstash_hosts,
+  $logstash_loadbalance = $::filebeat::logstash_loadbalance,
+  $modules_location     = $::filebeat::modules_location,
+  $shipper_name         = $::filebeat::shipper_name,
 ){
 
   file { $home_path:
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,20 +7,21 @@
 # @example
 #   include filebeat
 class filebeat (
-  String                                     $config_dir          = $config_dir,
-  Boolean                                    $config_reload       = $config_reload,
-  Optional[Array[String]]                    $elasticsearch_hosts = undef,
-  Variant[String, Enum['present', 'absent']] $ensure              = $ensure,
-  Optional[String]                           $inputs_location     = undef,
-  String                                     $home_path           = $home_path,
-  Optional[Array[String]]                    $logstash_hosts      = undef,
-  Boolean                                    $manage_repo         = $manage_repo,
-  Optional[String]                           $modules_location    = undef,
-  String                                     $repo_url            = $repo_url,
-  Enum['enabled', 'disabled', 'running']     $service_ensure      = $service_ensure,
-  String                                     $shipper_name        = $shipper_name,
-  Boolean                                    $validate_config     = $validate_config,
-  String                                     $validate_cmd        = $validate_cmd,
+  String                                     $config_dir           = $config_dir,
+  Boolean                                    $config_reload        = $config_reload,
+  Optional[Array[String]]                    $elasticsearch_hosts  = undef,
+  Variant[String, Enum['present', 'absent']] $ensure               = $ensure,
+  Optional[String]                           $inputs_location      = undef,
+  String                                     $home_path            = $home_path,
+  Optional[Array[String]]                    $logstash_hosts       = undef,
+  Boolean                                    $logstash_loadbalance = $logstash_loadbalance,
+  Boolean                                    $manage_repo          = $manage_repo,
+  Optional[String]                           $modules_location     = undef,
+  String                                     $repo_url             = $repo_url,
+  Enum['enabled', 'disabled', 'running']     $service_ensure       = $service_ensure,
+  String                                     $shipper_name         = $shipper_name,
+  Boolean                                    $validate_config      = $validate_config,
+  String                                     $validate_cmd         = $validate_cmd,
 ){
 
   class { '::filebeat::install': }
