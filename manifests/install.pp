a45fc2ab18642b5cdeb4382c27bd75c106045be8
Thomas Cassaert <tcassaert@inuits.eu>
2019-01-17 12:42:06 +0100

    Added support to install filebeat on debian based machines

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,19 +12,49 @@ class filebeat::install (
   $repo_url    = $::filebeat::repo_url,
 ){
   if $manage_repo {
-    if !defined(Yumrepo['elastic']) {
-      yumrepo { 'elastic':
-        descr    => 'Repository for Elastic rpms',
-        enabled  => 1,
-        baseurl  => $repo_url,
-        gpgcheck => '1',
-        gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+    case $::osfamily {
+      'Debian': {
+        Class['apt::update'] -> Package['filebeat']
+
+        if !defined(Apt::Source['elastic']){
+          apt::source { 'elastic':
+            ensure   => 'present',
+            location => $repo_url,
+            release  => 'stable',
+            repos    => 'main',
+            key      => {
+              id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+              source => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+            },
+          }
+        }
+
+        package { 'filebeat':
+          ensure  => $ensure,
+          require => Apt::Source['elastic'],
+        }
+      }
+      'RedHat': {
+        if !defined(Yumrepo['elastic']) {
+          yumrepo { 'elastic':
+            descr    => 'Repository for Elastic rpms',
+            enabled  => 1,
+            baseurl  => $repo_url,
+            gpgcheck => '1',
+            gpgkey   => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+          }
+        }
+
+        package { 'filebeat':
+          ensure  => $ensure,
+          require => Yumrepo['elastic'],
+        }
       }
     }
   }
-
-  package { 'filebeat':
-    ensure  => $ensure,
-    require => Yumrepo['elastic'],
-  }
+  else {
+    package { 'filebeat': 
+      ensure => $ensure,
+    }
+  }   
 }
